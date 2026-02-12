0ac5985e14994526869c88f3d17033c319af8b94
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2026-02-11 20:59:23 -0600

    Images are the worst thing ever

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -2,6 +2,8 @@
 
 import { TextLink } from 'solito/link'
 import { Text, View } from 'react-native'
+import { SolitoImage } from 'solito/image'
+import logoImage from 'app/assets/images/hackmty-logo.webp'
 
 export function HomeScreen() {
   return (
@@ -14,37 +16,14 @@ export function HomeScreen() {
         gap: 32,
       }}
     >
-      <H1>Welcome to Solito.</H1>
+      <SolitoImage 
+        src={logoImage} 
+        height={200}
+        width={100}
+        alt={"The HackMTY Logo"}
+      />      
+      <H1>HackMTY</H1>
       <View style={{ maxWidth: 600, gap: 16 }}>
-        <Text style={{ textAlign: 'center' }}>
-          Here is a basic starter to show you how you can navigate from one
-          screen to another. This screen uses the same code on Next.js and React
-          Native.
-        </Text>
-        <Text style={{ textAlign: 'center' }}>
-          Solito is made by{' '}
-          <TextLink
-            href="https://twitter.com/fernandotherojo"
-            target="_blank"
-            rel="noreferrer"
-            style={{ color: 'blue' }}
-          >
-            Fernando Rojo
-          </TextLink>
-          .
-        </Text>
-      </View>
-      <View style={{ flexDirection: 'row', gap: 32 }}>
-        <TextLink
-          href="/users/fernando"
-          style={{
-            fontSize: 16,
-            fontWeight: 'bold',
-            color: 'blue',
-          }}
-        >
-          Link
-        </TextLink>
       </View>
     </View>
   )
