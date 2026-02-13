90f6fc72ae47125958ce989e09bacc6701033e89
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2026-02-12 23:02:59 -0600

    elevation changes

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 10 },
         shadowOpacity: 0.4,
         shadowRadius: 8,
-        elevation: 8,      
+        elevation: 2,      
       },
       web: {
         filter: 'drop-shadow(0px 10px 8px rgba(0, 0, 0, 0.4))',
